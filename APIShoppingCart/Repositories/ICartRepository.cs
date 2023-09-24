db006d0f7b4646ccde0611fc01bbad3e2e6cb385
Juan Romero Dev <juancarlosromerodev@gmail.com>
2023-09-24 11:10:56 -0500

    Update ICartRepository with CRUD methods in the interface

--- APIShoppingCart/Repositories/ICartRepository.cs
+++ APIShoppingCart/Repositories/ICartRepository.cs
@@ -4,6 +4,10 @@ namespace APIShoppingCart.Repositories
 {
     public interface ICartRepository
     {
+        public Task<IEnumerable<Order>> GetOrders();
+        Task<Order> GetOrderrById(int id);
         public Task<bool> PlaceOrder(Order order);
+        Task<Order> UpdateOrder(Order order);
+        Task<bool> DeleteOrder(int id);
     }
 }
